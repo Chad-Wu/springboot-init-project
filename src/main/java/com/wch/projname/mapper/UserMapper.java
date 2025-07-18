55d2cf25bfa429472dcb80545d6c8efaa339a3c0
Chad-Wu <wch18017925219@163.com>
2025-07-18 15:55:59 +0800

    feat: 搭建初始化项目

---
+++ src/main/java/com/wch/projname/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.wch.projname.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wch.projname.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author wch  
+ * @from  chonghe  
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
